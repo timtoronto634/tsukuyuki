d454a34aefcd58b253e64858e5c01002a94e255f
timtoronto634 <tim225x@gmail.com>
2022-10-02 07:28:57 +0900

    Add Before state logic

--- Assets/Scripts/GodOfBefore.cs
+++ Assets/Scripts/GodOfBefore.cs
@@ -5,21 +5,54 @@ using UnityEngine.SceneManagement;
 
 public class GodOfBefore : MonoBehaviour
 {
-    public gifPlayer image;
-    
-    public AudioSource tsukutte;
-    public AudioSource countdown;
+    public enum State {
+        init,
+        hyokkori,
+        countdown
+    }
+    public State state;
+
+    public GameObject hyokkoriImage;
+    public GameObject hukidashi;
+    public GameObject tsukutte;
+
+    public GameObject countdownGif;
+    public GameObject countdownAudio;
     // Start is called before the first frame update
     void Start()
     {
-        tsukutte.Play();
-        image.Play();
-        countdown.Play();
+        state = State.init;
+        played = false;
     }
+    private bool played;
 
     // Update is called once per frame
     void Update()
     {
+        switch (state)
+        {
+            case State.init:
+                state = State.hyokkori;
+                break;
+            case State.hyokkori:
+                if (!played && !tsukutte.GetComponent<AudioSource>().isPlaying) {
+                    hyokkoriImage.SetActive(true);
+                    hukidashi.SetActive(true);
+                    tsukutte.SetActive(true);
+                    played = true;
+                } else if (played &&  !tsukutte.GetComponent<AudioSource>().isPlaying) {
+                    state = State.countdown;
+                    hyokkoriImage.SetActive(false);
+                    hukidashi.SetActive(false);
+                }
+                break;
+            case State.countdown:
+                countdownGif.SetActive(true);
+                countdownGif.GetComponent<gifPlayer>().Play();
+                countdownAudio.SetActive(true);
+                break;
+            default:
+                break;
+        }
     }
-
 }
